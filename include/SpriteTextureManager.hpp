fbaefc01dd7c79cef5ff9632da2c31aa87965fdc
FromAlaska <Jim_samson1335@yahoo.com>
2017-11-28 21:53:43 -0900

    fdsa

---
+++ include/SpriteTextureManager.hpp
@@ -0,0 +1,48 @@
+#ifndef SPRITETEXTUREMANAGER_HPP
+#define SPRITETEXTUREMANAGER_HPP
+
+#include "Display.hpp"
+
+#include <stdio.h>
+#include <string>
+using std::string;
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+namespace Corn
+{
+	class Sprite
+	{
+	public:
+		Sprite()
+		{
+			int ELEMENT_SIZE = 2;
+			_positionSpeed.resize(ELEMENT_SIZE);
+		}
+
+		void loadTexture(string &&, int, int, int, int);
+		void draw();
+		void setPosition(int);
+
+		void loadTexture(std::vector<Corn::Sprite> &, string &&);
+		void updateObject(std::vector<Corn::Sprite> &);
+		void setPositionObj(std::vector<Corn::Sprite>&,const int &);
+		void moveObject(std::vector<Corn::Sprite> &, sf::Time &, const int &);
+		void drawObject(std::vector<Corn::Sprite> &);
+
+		sf::Sprite _spriteObject;
+		sf::Texture _textureObject;
+
+		const int X_POSITION = 272;
+		const int Y_POSITION = 160;
+
+	private:
+		std::vector<sf::Vector2f> _positionSpeed;
+	};
+}
+
+#endif // SPRITETEXTUREMANAGER_HPP
---
+++ src/SpriteTextureManager.cpp
@@ -0,0 +1,72 @@
+#include "../include/SpriteTextureManager.hpp"
+
+namespace frontier
+{
+	void Sprite::loadTexture(string && nameOfFile, int scaleX, int scaleY, int posX, int posY)
+	{
+		if (!_textureObject.loadFromFile(resourcePath() + nameOfFile))
+		{ std::cout << "cannot find " << nameOfFile << std::endl; }
+
+		_spriteObject.setTexture(_textureObject);
+		_spriteObject.setPosition(posX, posY);
+	}
+
+	void Sprite::loadTexture(std::vector<frontier::Sprite> & obj, string && nameOfFile)
+	{
+		for(auto iterator = obj.begin(); iterator != obj.end(); ++iterator)
+		{
+			if (!iterator->_textureObject.loadFromFile(resourcePath() + nameOfFile))
+			{ std::cout << "cannot load " << nameOfFile << std::endl; }
+
+			iterator->_spriteObject.setTexture(iterator->_textureObject);
+
+			for(int elementScope = 0; elementScope <= obj.size(); ++elementScope)
+			{
+				obj[elementScope]._spriteObject.setPosition(X_POSITION * elementScope-60,0);
+			}
+		}
+	}
+
+	void Sprite::draw()
+	{
+		Display::draw(_spriteObject);
+	}
+
+	void Sprite::updateObject(std::vector<frontier::Sprite> & sprObj)
+	{
+		int iterator = 0;
+
+		for(auto index = _positionSpeed.begin(); index != _positionSpeed.end(); ++index)
+		{
+			*index = sprObj[iterator]._spriteObject.getPosition();
+
+			if((index->x) >= X_POSITION)
+			{
+				sprObj[iterator]._spriteObject.setPosition(-543,0);
+			}
+
+			iterator++;
+
+			if(iterator > 3)
+			{
+				iterator = 0;
+			}
+		}
+	}
+
+	void Sprite::moveObject(std::vector<frontier::Sprite> & obj, sf::Time & dt, const int & objSpeed)
+	{
+		for(auto index = obj.begin(); index != obj.end(); ++index)
+		{
+			index->_spriteObject.move(objSpeed * dt.asSeconds(),0);
+		}
+	}
+
+	void Sprite::drawObject(std::vector<frontier::Sprite> & obj)
+	{
+		for(auto index = obj.begin(); index != obj.end(); ++index)
+		{
+			Display::draw(index->_spriteObject);
+		}
+	}
+}
